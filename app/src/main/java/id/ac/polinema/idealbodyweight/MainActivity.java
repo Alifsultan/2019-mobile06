41d2136da3bcc2cb47da6a3a3415bd7ce5fc930d
alifsultan <alifsultan22@gmail.com>
2019-10-05 02:00:52 +0700

    membuat result fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -9,19 +9,23 @@ import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.Fragment.AboutFragment;
 import id.ac.polinema.idealbodyweight.Fragment.MenuFragment;
+import id.ac.polinema.idealbodyweight.Fragment.ResultFragment;
 import id.ac.polinema.idealbodyweight.util.BrocaIndexFragment;
 
-public abstract class MainActivity extends AppCompatActivity implements
+public class MainActivity extends AppCompatActivity implements
 		MenuFragment.OnFragmentInteractionListener,
-		BrocaIndexFragment.OnFragmentInteractionListener {
+		BrocaIndexFragment.OnFragmentInteractionListener,
+		ResultFragment.OnFragmentInteractionListener {
 
 
 	// Deklarasikan atribut Fragment di sini
 	private AboutFragment aboutFragment;
 	private BrocaIndexFragment brocaIndexFragment;
+	private ResultFragment resultFragment;
 
-	public MainActivity(BrocaIndexFragment brocaIndexFragment) {
+	public MainActivity(BrocaIndexFragment brocaIndexFragment, ResultFragment resultFragment) {
 		this.brocaIndexFragment = brocaIndexFragment;
+		this.resultFragment = resultFragment;
 	}
 
 
@@ -30,6 +34,20 @@ public abstract class MainActivity extends AppCompatActivity implements
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		aboutFragment = AboutFragment.newInstance("Alif Mohammad Sultan S");
+		resultFragment = new ResultFragment();
+	}
+	@Override
+	public void onCalculateBrocaIndexClicked(float index) {
+		resultFragment.setInformation(String.format("Your ideal weight is %.2f kg", index));
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, resultFragment)
+				.commit();
+	}
+	@Override
+	public void onTryAgainButtonClicked(String tag) {
+		getSupportFragmentManager().beginTransaction()
+				.replace(R.id.fragment_container, brocaIndexFragment)
+				.commit();
 	}
 
 	@Override
